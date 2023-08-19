3c6d4e4add59bbff5f4d6a5b1ffcd61063e2c397
ayushkr7 <ayushkumar140699@gmail.com>
2023-08-19 11:45:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+// const resolve = require("resolve");
+
+// resolve.fallback: { "path": false }
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <br />
+            <Routes>
+                <Route path="/" exact element={<ExercisesList />} />
+                <Route path="edit/:id" element={<EditExercise />} />
+                <Route path="create" element={<CreateExercise />} />
+                <Route path="user" element={<CreateUser />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
